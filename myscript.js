7f803cfa188bad5b62b2531502c37f844b11ce14
amirhossain97 <75106745+amirhossain97@users.noreply.github.com>
2020-11-28 13:45:44 +0600

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:8,
+        margin:10,
+        loop:true,
+        autoplay:true,
+        auteplayTimeout:1000,
+         
+    });
+});
